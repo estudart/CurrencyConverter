d9145938279b8373a696eda47f56af3c4e07b2cd
estudart <erico.studartdf@gmail.com>
2024-01-14 16:37:55 -0300

    initial part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [convertion, setConvertion] = useState(0);
+  const [firstCurrency, setFirstCurrency] = useState("BRL");
+  const [secondCurrency, setSecondCurrency] = useState("USD");
+  const [amount, setAmount] = useState(0);
+
+  function handleSelectFirst(value) {
+    setFirstCurrency(value);
+  }
+  function handleSelectSecond(value) {
+    setSecondCurrency(value);
+  }
+  function handleAmount(value) {
+    setAmount(value);
+  }
+  useEffect(
+    function () {
+      async function showConvertion() {
+        const result = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${firstCurrency}&to=${secondCurrency}`
+        );
+        const data = await result.json();
+        const test = data.rates[Object.keys(data.rates)[0]];
+        console.log(test);
+        setConvertion(test);
+      }
+      showConvertion();
+    },
+    [firstCurrency, secondCurrency, amount]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={amount} onChange={(e) => handleAmount(e.target.value)} />
+      <select onChange={(e) => handleSelectFirst(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="BRL">BRL</option>
+        <option value="EUR">EUR</option>
+      </select>
+      <select onChange={(e) => handleSelectSecond(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="BRL">BRL</option>
+        <option value="EUR">EUR</option>
+      </select>
+      <p>{convertion}</p>
     </div>
   );
 }
