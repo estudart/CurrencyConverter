f7e3552e741a974cdeeff49c2e144cce7a575095
estudart <erico.studartdf@gmail.com>
2024-01-15 19:07:04 -0300

    adding some style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [firstCurrency, setFirstCurrency] = useState("BRL");
   const [secondCurrency, setSecondCurrency] = useState("USD");
   const [amount, setAmount] = useState(0);
+  const [validAmount, setValidAmount] = useState(false);
 
   function handleSelectFirst(value) {
     setFirstCurrency(value);
@@ -26,40 +27,99 @@ function App() {
         const data = await result.json();
         const test = Object.values(data.rates)[0];
         console.log(test);
-        setConversion(test);
+        setConversion(test.toFixed(2));
       }
 
       if (firstCurrency !== secondCurrency) {
         if (isNaN(amount) || parseInt(amount) === 0 || amount.trim() === "") {
           console.log(amount);
+          setValidAmount(false);
           setConversion("Choose an amount to convert");
         } else {
+          setValidAmount(true);
           showConversion();
         }
       } else {
+        setValidAmount(false);
         setConversion("Currencies are the same");
       }
     },
     [firstCurrency, secondCurrency, amount]
   );
   return (
-    <div>
-      <input value={amount} onChange={(e) => handleAmount(e.target.value)} />
-      <select onChange={(e) => handleSelectFirst(e.target.value)}>
-        <option value="USD">USD</option>
-        <option selected value="BRL">
-          BRL
-        </option>
-        <option value="EUR">EUR</option>
-      </select>
-      <select onChange={(e) => handleSelectSecond(e.target.value)}>
-        <option selected value="USD">
-          USD
-        </option>
-        <option value="BRL">BRL</option>
-        <option value="EUR">EUR</option>
-      </select>
-      <p>{conversion}</p>
+    <div className="conversion-form">
+      <h2>Pocket Exchange</h2>
+      <img id="gif" src="./money.gif" alt="money gif" />
+      <div className="currency-price">
+        <p>Set a price to convert:</p>
+        <input
+          id="conversionAmount"
+          style={{ width: "50px", textAlign: "center" }}
+          value={amount}
+          onChange={(e) => handleAmount(e.target.value)}
+        />
+        <div
+          style={{
+            display: "flex",
+            marginTop: "3%",
+            borderTop: "1px solid red",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p>From: </p>
+          <label class="select">
+            <select
+              id="box"
+              className="select"
+              onChange={(e) => handleSelectFirst(e.target.value)}
+            >
+              <option value="USD">USD</option>
+              <option selected value="BRL">
+                BRL
+              </option>
+              <option value="EUR">EUR</option>
+              <option value="CNY">CNY</option>
+              <option value="INR">INR</option>
+              <option value="JPY">JPY</option>
+              <option value="GBP">GBP</option>
+              <option value="ILS">ILS</option>
+            </select>
+          </label>
+        </div>
+        <img
+          id="conversion-icon"
+          src="./conversion-icon.png"
+          alt="conversion icon"
+        />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p>To: </p>
+          <select
+            id="secondCurrency"
+            onChange={(e) => handleSelectSecond(e.target.value)}
+          >
+            <option selected value="USD">
+              USD
+            </option>
+            <option value="BRL">BRL</option>
+            <option value="EUR">EUR</option>
+            <option value="CNY">CNY</option>
+            <option value="INR">INR</option>
+            <option value="JPY">JPY</option>
+            <option value="GBP">GBP</option>
+            <option value="ILS">ILS</option>
+          </select>
+        </div>
+      </div>
+      {validAmount ? <p>Amount: {conversion}</p> : <p>{conversion}</p>}
     </div>
   );
 }
